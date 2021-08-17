ebaded1c5940f8f7c139e66eeb3bf31ffaaaf0d0
haglea <haglea@icloud.com>
2021-08-18 01:13:31 +0200

    cors

--- src/server.js
+++ src/server.js
@@ -10,6 +10,21 @@ const server = express();
 const port = process.env.PORT;
 console.log(process.env)
 const publicFolderPath = join(process.cwd(), "public")
+
+const whitelist= [process.env.FE_DEV_URL, process.env.FE_PROD_URL] // what frontends are allowed
+
+const corsOpts = {
+  origin: function(origin, next){
+    console.log('ORIGIN --> ', origin)
+    if(whitelist.indexOf(origin) !== -1){ // if received origin is in the whitelist I'm going to allow that request
+      next(null, true) //no error
+    }else{ // if it is not, I'm going to reject that request
+      next(new Error(`Origin ${origin} not allowed!`))
+    }
+  }
+}
+
+
 // ***************** MIDDLEWARES *******************************
 
 const loggerMiddleware = (request, response, next) => {
@@ -20,7 +35,7 @@ const loggerMiddleware = (request, response, next) => {
 // GLOBAL LEVEL MIDDLEWARES
 server.use(loggerMiddleware)
 server.use(express.static(publicFolderPath))
-server.use(cors())
+server.use(cors(corsOpts))
 server.use(express.json()) //this comes before the routes
 server.use("/authors", authorsRouter)
 server.use("/blogPosts", blogPostsRouter)
